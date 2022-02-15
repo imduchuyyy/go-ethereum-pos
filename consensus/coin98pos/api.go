1f8ac4b914dbfbeb1029742bd23b485b8c6af244
Bui Duc Huy <duchuy.124dk@gmail.com>
2022-02-15 17:01:18 +0700

    feat(coin98 pos api): getsnapshot interface

--- consensus/coin98pos/api.go
+++ consensus/coin98pos/api.go
@@ -2,9 +2,15 @@ package coin98pos
 
 import (
   "github.com/ethereum/go-ethereum/consensus"
+  "github.com/ethereum/go-ethereum/rpc"
 )
 
 type API struct {
   chain consensus.ChainHeaderReader
   coin98pos *Coin98Pos
+}
+
+// GetSnapshot retrieves the state snapshot at a given block.
+func (api *API) GetSnapshot(number *rpc.BlockNumber) error {
+  return nil
 }
