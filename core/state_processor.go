61996a28339a87c2fb207cfe64668b20284aa767
Bui Duc Huy <duchuy.124dk@gmail.com>
2022-02-23 09:31:11 +0700

    feat(contract gas pay): test call contract inside geth

--- core/state_processor.go
+++ core/state_processor.go
@@ -66,10 +66,12 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 		allLogs     []*types.Log
 		gp          = new(GasPool).AddGas(block.GasLimit())
 	)
+  // test check contract enable pay gas
 	// Mutate the block and state according to any hard-fork specs
 	if p.config.DAOForkSupport && p.config.DAOForkBlock != nil && p.config.DAOForkBlock.Cmp(block.Number()) == 0 {
 		misc.ApplyDAOHardFork(statedb)
 	}
+
 	blockContext := NewEVMBlockContext(header, p.bc, nil)
 	vmenv := vm.NewEVM(blockContext, vm.TxContext{}, statedb, p.config, cfg)
 	// Iterate over and process the individual transactions
@@ -102,6 +104,10 @@ func applyTransaction(msg types.Message, config *params.ChainConfig, bc ChainCon
 	if err != nil {
 		return nil, err
 	}
+  testContract := common.HexToAddress("0x856C2afd19b368DD38449C751f831FedEa9542fc")
+  testMethod := []byte("0x0be3fb78")
+  copyState := statedb.Copy()
+  fmt.Printf("Msg", isContractEnablePayGas(copyState, testContract, testMethod))
 
 	// Update the state with pending changes.
 	var root []byte
